361385c7405ba701cf84dc7b14fb222faf3520b2
Nivedita <142734416+nivds08@users.noreply.github.com>
2026-04-09 23:45:25 +0530

    Time: 0 ms (100%), Space: 8.5 MB (49.61%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,10 +1,19 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        int res = 1;
-        while (res <= x / res) {
-            res++;
+        long long low = 0, high = x, ans = 0;
+
+        while (low <= high) {
+            long long mid = (low + high) / 2;
+
+            if (mid * mid <= x) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
         }
-        return res - 1;
+
+        return ans;
     }
 };
